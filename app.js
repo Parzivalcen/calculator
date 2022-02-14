1694a7d622d78ced8015fcd486ce7d04c6e61173
David Centurion <dencen9@gmail.com>
2022-02-14 15:33:51 -0300

    backsapce is working

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 // show buttons pressed value on screen
 // get the buttons value using query selector and event listener
-// We append this to our number element <<p>> variable 
-// then we append the <<p>> to our screen div. 
-
+// We append this to our number element <<p>> variable
+// then we append the <<p>> to our screen div.
 
 // Show the button value in the screen
-const btns = document.querySelectorAll('.btn-c');
-const screen = document.querySelector('.screen');
-let number = document.createElement('p')
-number.classList.add('numArray')
+const btns = document.querySelectorAll(".btn-c");
+const screen = document.querySelector(".screen");
+let number = document.createElement("p");
+number.classList.add("numArray");
 // console.log(btns);
 
 btns.forEach((e) => {
-  e.addEventListener('click', event =>{
+  e.addEventListener("click", (event) => {
     let btnValue = event.target.innerText;
-    btnValue = document.createTextNode(btnValue)
-    number.appendChild(btnValue)
-    screen.appendChild(number)
-    console.log(number.innerHTML)
-  } )
+    btnValue = document.createTextNode(btnValue);
+    number.appendChild(btnValue);
+    screen.appendChild(number);
+    console.log(number.innerHTML);
+  });
 });
 
-const backSpace = document.querySelector('#delete');
-backSpace.addEventListener('click', event => {
-  number = backspace(number.innerHTML);
-  number = document.createTextNode(btnValue)
-
-  screen.appendChild(number)
-
-})
-function backspace(array){
-  array = array.slice(0, array.length -1)
-  return array;
-
+const back = document.querySelector("#delete");
+back.addEventListener("click", (event) => {
+  backspace();
+  // back = document.createTextNode(backspace(number.innerHTML));
+  // number.removeChild(btnValue);
+  // number.appendChild(back);
+});
+function backspace() {
+  let value = document.querySelector(".numArray").innerHTML;
+  document.querySelector(".numArray").innerHTML = value.slice(
+    0,
+    value.length - 1
+  );
 }
 
-function showNumber(e){
+function showNumber(e) {
   btnValue = e.innerText;
   console.log(btnValue);
 }
